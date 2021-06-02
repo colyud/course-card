480f48d113f8e3725902552eec06ed4546a66913
colyud <33720136+colyud@users.noreply.github.com>
2021-06-02 17:46:58 -0500

    create course card

---
+++ src/data.js
@@ -0,0 +1,113 @@
+const data = [
+    {
+        id: 1,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 2,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 3,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 4,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 5,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 6,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 7,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 8,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 9,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+    {
+        id: 10,
+        courseName: "From figma to code",
+        teacher: {
+            name: "Gary Simon",
+            img: "https://scrimba.com/avatars/designcourse/64",
+        },
+        lessons: 42,
+        duration: 180,
+        level: "Intermedia",
+    },
+];
+export default data;
